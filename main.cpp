1286dd4113ad9fef17c4eaa7c74fcffebc6e0f5e
Zach Dobbs <dobbszach@gmail.com>
2019-04-30 16:15:19 -0500

    Create 2d version

--- main.cpp
+++ main.cpp
@@ -12,168 +12,354 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "Angel.h"
 
-typedef vec4 point4;
-typedef vec4 color4;
+const int width = 450, height = 450;
+const int RacketWidth = 10, RacketHeight = 100;
+const int BallWidth = 10, BallHeight = 10;
 
-GLuint vao;
-int Index = 0;
-const int NumVertices = 36;
-point4 points[NumVertices];
-color4 colors[NumVertices];
+double gameTime;
 
-// Translation will hold the current translation modifier
-// { x, y, _, z }
-GLfloat  Translation[4] = { 0.0, 0.0, 0.0, 0.0 };
+int LeftY = 0;
+int RightY = 0;
+int Racketspeed = 5;
 
-GLuint  translation; // The location of the "translation" shader uniform shader variable
+int ballX, ballY;
+int ballVelocityX = 1;
+float ballVelocityY = 0.0f;
+float StoredVelocity = 0.0f;
 
-// define vertices of ball
-point4 ballVertices[8] = {
-    point4(-0.1, -0.1, 0.1, 1.0),
-    point4(-0.1, 0.1, 0.1, 1.0),
-    point4(0.1, 0.1, 0.1, 1.0),
-    point4(0.1, -0.1, 0.1, 1.0),
-    point4(-0.1, -0.1, -0.1, 1.0),
-    point4(-0.1, 0.1, -0.1, 1.0),
-    point4(0.1, 0.1, -0.1, 1.0),
-    point4(0.1, -0.1, -0.1, 1.0)
+// ball velocity
+double TimeSpeed=0.2;
+
+int LeftScore = 0;
+int RightScore = 0;
+
+void Reset();
+void Collisions();
+bool Between(int target, int min, int max);
+
+class gl_helper {
+    public:
+        static void DrawRect(int x, int y, int width, int height);
+        static void DrawNumber(int x , int y, int n);
+        static void DebugPoint(float x , float y);
 };
 
-// vertex colors.. default to white for now
-color4 vertex_colors[8] = {
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0),  // white
-    color4(1.0, 1.0, 1.0, 1.0)   // white
-};
-
-// coloring cube
-void quad(int a, int b, int c, int d, point4* vertices) {
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-    colors[Index] = vertex_colors[b]; points[Index] = vertices[b]; Index++;
-    colors[Index] = vertex_colors[c]; points[Index] = vertices[c]; Index++;
-    colors[Index] = vertex_colors[a]; points[Index] = vertices[a]; Index++;
-    colors[Index] = vertex_colors[c]; points[Index] = vertices[c]; Index++;
-    colors[Index] = vertex_colors[d]; points[Index] = vertices[d]; Index++;
+void gl_helper::DebugPoint(float x, float y) {
+    
+    float SavedColor[4];
+    glGetFloatv(GL_CURRENT_COLOR, SavedColor);
+    glColor4f(1.0f, 0, 0, 1.0f);
+    
+    glBegin(GL_POINTS);
+    glVertex2f(x, y);
+    glEnd();
+    
+    glColor4f(SavedColor[0],SavedColor[1],SavedColor[2],SavedColor[3]);
 }
 
-// color the ball using quad
-void colorBall() {
-    quad(1, 0, 3, 2, ballVertices);
-    quad(2, 3, 7, 6, ballVertices);
-    quad(3, 0, 4, 7, ballVertices);
-    quad(6, 5, 1, 2, ballVertices);
-    quad(4, 5, 6, 7, ballVertices);
-    quad(5, 4, 8, 1, ballVertices);
+void gl_helper::DrawRect(int x, int y, int width, int height) {
+    glBegin(GL_QUADS);
+    glVertex2f(x, y);
+    glVertex2f(x + width, y);
+    glVertex2f(x + width, y + height);
+    glVertex2f(x, y + height);
+    glEnd();
 }
 
-void init(void) {
-    colorBall();
-    
-    // create a vertex array object
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    
-    // create and initialize buffer object
-    // NOTE how can this be reused to also generate the bumpers?
-    GLuint buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(points)+sizeof(colors), NULL, GL_STATIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(points), points);
-    glBufferSubData(GL_ARRAY_BUFFER, sizeof(points), sizeof(colors), colors);
-    
-    // load shaders
-    // NOTE for right now make this a relative path
-    // TODO fix this to work without making a specific path
-    GLuint program = InitShader("/Users/zachdobbs/Documents/Computer Graphics/FinalProject/GraphicsFinalProject/GraphicsFinalProject/vshader36.glsl", "/Users/zachdobbs/Documents/Computer Graphics/FinalProject/GraphicsFinalProject/GraphicsFinalProject/fshader36.glsl");
-    glUseProgram(program);
-    
-    // set up vertex arrays
-    GLuint vPosition = glGetAttribLocation(program, "vPosition");
-    glEnableVertexAttribArray(vPosition);
-    glVertexAttribPointer(vPosition, 4, GL_FLOAT, GL_FALSE, 0,
-                          BUFFER_OFFSET(0));
-    
-    GLuint vColor = glGetAttribLocation(program, "vColor");
-    glEnableVertexAttribArray(vColor);
-    glVertexAttribPointer(vColor, 4, GL_FLOAT, GL_FALSE, 0,
-                          BUFFER_OFFSET(sizeof(points)));
-    
-    translation = glGetUniformLocation(program, "translation");
-    
-    glEnable(GL_DEPTH_TEST);
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-}
-
-// key callback handler
-void mykey(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (action == GLFW_PRESS) {
-        switch(key) {
-            case GLFW_KEY_ESCAPE:
-                glfwSetWindowShouldClose(window, GL_TRUE);
-                break;
-            case GLFW_KEY_W:
-                Translation[0] += 1.0;
-                break;
-        }
+void gl_helper::DrawNumber(int x, int y, int n) {
+    switch(n) {
+        case 0:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x, y + 30, 5, 20);
+            DrawRect(x + 25, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5 );
+            break;
+            
+        case 1:
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 25, y + 30, 5, 20);
+            break;
+            
+        case 2:
+            DrawRect(x + 5, y , 20, 5);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5);
+            break;
+            
+        case 3:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x + 25, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5);
+            break;
+            
+        case 4:
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x + 25, y + 30, 5, 20);
+            break;
+            
+        case 5:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x + 25, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5);
+            break;
+            
+        case 6:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x, y + 30, 5, 20);
+            DrawRect(x + 25, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5);
+            break;
+            
+        case 7:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 25, y + 30, 5, 20);
+            break;
+            
+        case 8:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x, y + 30, 5, 20);
+            DrawRect(x + 25, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5);
+            break;
+            
+        case 9:
+            DrawRect(x + 5, y, 20, 5);
+            DrawRect(x, y + 5, 5, 20);
+            DrawRect(x + 25, y + 5, 5, 20);
+            DrawRect(x + 5, y + 25, 20, 5);
+            DrawRect(x + 25, y + 30, 5, 20);
+            DrawRect(x + 5, y + 50, 20, 5);
+            break;
     }
 }
 
-int main(int argc, const char * argv[]) {
-    GLFWwindow *window = NULL;
-    const GLubyte *renderer;
-    const GLubyte *version;
-    if (!glfwInit()) {
-        fprintf(stderr, "ERROR: could not start GLFW3\n");
-        return 1;
+void mykey(GLFWwindow* window, int key, int scancode, int action, int mods){
+//    if (action == GLFW_KEY_PRESS) {
+//        switch(key) {
+//            default:
+//                break;
+//        }
+//    }
+    //Left Control UP
+    if(key == GLFW_KEY_UP) {
+        RightY-=Racketspeed;
     }
-    
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #endif
-    
-    window = glfwCreateWindow(640, 640, "Pong", NULL, NULL);
-    if (!window) {
-        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
-        glfwTerminate();
-        return 1;
+    //Left Control Down
+    if(key == GLFW_KEY_DOWN) {
+        RightY+=Racketspeed;
     }
+    //Right Control UP
+    if(key == GLFW_KEY_W) {
+        LeftY-=Racketspeed;
+    }
+    //Right Control Down
+    if(key == GLFW_KEY_S) {
+        LeftY += Racketspeed;
+    }
+}
+
+
+int main() {
+    GLFWwindow* window;
+    
+    glfwInit();
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    window = glfwCreateWindow(width, height, "Pong", NULL, NULL);
     glfwMakeContextCurrent(window);
-    glewExperimental = GL_TRUE;
-    glewInit();
-    renderer = glGetString(GL_RENDERER); /* get renderer string */
-    version = glGetString(GL_VERSION);     /* version as a string */
-    printf("Renderer: %s\n", renderer);
-    printf("OpenGL version supported %s\n", version);
-    init();
     
-    // setup the callbacks
     glfwSetKeyCallback(window, mykey);
     
-    do {
-        // clear window
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-        
-        // TODO do the rest of stuff here..
-        glUniform4fv(translation, 1, Translation);
-        
-        glDrawArrays(GL_TRIANGLES, 0, NumVertices);
-        
-        glfwPollEvents();
-        glfwSwapBuffers(window);
-    } while(!glfwWindowShouldClose(window));
+    //Ball Starting position
+    ballX = width/2 - 5 ;
+    ballY = height/2 - 5;
     
+    //Racket Starting Y Position
+    LeftY = height/2 - 50;
+    RightY = height/2 - 50;
+    
+    gameTime = glfwGetTime() * TimeSpeed;
+    
+    
+    while(!glfwWindowShouldClose(window)){
+        
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0 , width , height , 0 , 0 , 1);
+        glDisable(GL_DEPTH_TEST);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        
+        glClearColor(0.0f, 0.0f, 0.0f, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        //Collision Detection
+        Collisions();
+        
+        //Left Racket
+        gl_helper::DrawRect( 0 , LeftY, RacketWidth , RacketHeight  );
+        //Right Racket
+        gl_helper::DrawRect( width - 10 , RightY , RacketWidth , RacketHeight  );
+        //Ball
+        gl_helper::DrawRect( ballX , ballY , BallWidth , BallHeight  );
+        
+        //Left Score
+        gl_helper::DrawNumber( width/2 - 40 , 10 , LeftScore );
+        //Right Score
+        gl_helper::DrawNumber( width/2 + 10 , 10 , RightScore );
+        //Colon
+        gl_helper::DrawRect( width/2 - 2 , 25 , 5 , 5 );
+        gl_helper::DrawRect( width/2 - 2 , 45 , 5 , 5 );
+        
+        glfwSwapBuffers(window);
+        glfwSwapInterval(1);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
     glfwTerminate();
+    
     return 0;
+}
+
+void Collisions(){
+    
+    if( glfwGetTime() - gameTime > 1.0 ){
+        
+        gameTime = glfwGetTime()*TimeSpeed;
+        
+        ballX += ballVelocityX;
+        
+        StoredVelocity += ballVelocityY;
+        
+        if(StoredVelocity > 1.0f){
+            ballY+=1;
+            StoredVelocity-=1;
+        }else if(StoredVelocity < -1.0f){
+            ballY-=1;
+            StoredVelocity+=1;
+            
+        }
+    }
+    
+    
+    //Ball at Right Edge
+    if( ballX > width - RacketWidth*2 ){
+        
+        if( Between(ballY, RightY + RacketHeight, RightY )  ){
+            
+            ballVelocityX= -ballVelocityX;
+            
+            if( Between(ballY, RightY, RightY + RacketHeight*0.3 ) ){
+                
+                ballVelocityY-=0.3;
+                
+            }else if( Between(ballY,  RightY + RacketHeight - RacketHeight*0.3 , RightY + RacketHeight ) ){
+                
+                ballVelocityY+=0.3;
+                
+            }
+            
+            
+        }else{
+            
+            LeftScore++;
+            Reset();
+            
+        }
+        
+        //Ball at Left Edge
+    }else if( ballX < RacketWidth ){
+        
+        if( Between(ballY, LeftY + RacketHeight, LeftY )  ){
+            
+            ballVelocityX= -ballVelocityX;
+            
+            if( Between(ballY, LeftY, LeftY + RacketHeight*0.3 ) ){
+                
+                ballVelocityY-=0.3;
+                
+            }else if( Between(ballY,  LeftY + RacketHeight - RacketHeight*0.3 , RightY + RacketHeight ) ){
+                
+                ballVelocityY+=0.3;
+            }
+            
+        }else{
+            
+            RightScore++;
+            Reset();
+            
+        }
+        
+        //Ball at Top Edge
+    }else if( ballY < 0 ){
+        
+        ballY = 0;
+        ballVelocityY= -ballVelocityY;
+        
+        //Ball at Buttom Edge
+    }else if( ballY > height - BallHeight ){
+        
+        ballY = height - BallHeight;
+        ballVelocityY= -ballVelocityY;
+        
+    }
+    
+}
+
+
+bool Between(int target, int a , int b ){
+    
+    int max;
+    int min;
+    
+    if( a > b){
+        
+        max = a;
+        min = b;
+        
+    }else{
+        max=b;
+        min=a;
+    }
+    
+    if( target < max && target > min)
+        return true;
+    
+    return false;
+    
+}
+
+void Reset(){
+    
+    ballX = width/2 - BallWidth/2 ;
+    glfwSetTime(0.0);
+    gameTime=0.0;
+    ballVelocityX=1;
+    ballVelocityY=0;
+    StoredVelocity=0;
+    
+    if(LeftScore > 9 || RightScore > 9){
+        
+        LeftScore = 0;
+        RightScore = 0;
+    }
 }
